17623b78bb6bad9cd9cf2e9cbc618a09462e28a7
Daniel Hara <haradaniel734@gmail.com>
2022-07-04 22:26:04 +0200

    bpm

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import clsx from 'clsx';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import styles from './App.module.scss';
 
 function App() {
@@ -7,9 +7,18 @@ function App() {
   const [beatsPerMinute, setBeatsPerMinute] = useState(120);
   const [active, setActive] = useState(0);
 
+  const interval = useRef(null);
+
   useEffect(() => {
-    setInterval(() => setActive(active => (active + 1) % numberOfBeats), 60 / beatsPerMinute * 1000);
-  }, []);
+    if (interval.current) {
+      clearInterval(interval.current);
+    }
+
+    console.log(`numberOfBeats = ${numberOfBeats}`);
+    console.log(`beatsPerMinute = ${beatsPerMinute}`);
+
+    interval.current = setInterval(() => setActive(active => (active + 1) % numberOfBeats), 60 / beatsPerMinute * 1000);
+  }, [numberOfBeats, beatsPerMinute]);
 
   const handleStopClick = () => {}
 
@@ -24,7 +33,18 @@ function App() {
         </div>
       </div>
 
-      <button onClick={handleStopClick} >
+      <label for="bpm">
+        Beats per Minute
+      </label>
+      <input id="bpm" onChange={(event) => {
+        const number = Number(event.target.value)
+        
+        if (number > 0) {
+          setBeatsPerMinute(number);
+        }
+      }} />
+
+      <button onClick={handleStopClick}>
         STOP
       </button>
     </div>
